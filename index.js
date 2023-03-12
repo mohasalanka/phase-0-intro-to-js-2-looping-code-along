4b02e3f57549f39ec6074f0c2a7fb29cd500a2bf
Mohamed <mohasalanka2020@gmail.com>
2023-03-12 18:13:48 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const myArr= [ 'Guadalupe', 'Ollie', 'Aki' ];
+
+function writeCards(arr, eventName) {
+    const emptyArr = [];
+    for(let i=0 ; i< arr.length ; i++) {
+        emptyArr.push(`Thank you, ${arr[i]}, for the wonderful surprise gift!`);
+    }
+    return emptyArr;
+}
+console.log(writeCards(myArr));
+
+function countDown(j){
+    while (j>=0) {
+        console.log(j);
+        j--;
+    }
+
+}
+countDown(10);
